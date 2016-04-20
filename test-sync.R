5853da8ff3540dab1853cdb70c60e021f41d107c
wush.wu <wush.wu@generalmobi.com>
2016-04-20 13:39:10 +0800

    use minibatch to process data

--- test-sync.R
+++ test-sync.R
@@ -5,18 +5,33 @@ ndlist <- dir("test/ndjson")
 for(fname in ndlist) {
   if (file.exists(dst.path <- file.path("test-sync", "R", fname))) file.remove(dst.path)
   dst <- file(dst.path, open = "wt")
-  src <- file(file.path("test/ndjson", fname))
+  src.path <- file.path("test/ndjson", fname)
+  src <- switch(tools::file_ext(src.path),
+                "gz" = gzfile(src.path),
+                file(src.path)
+  )
+  if (interactive()) {
+    .cmd <- sprintf("%s %s | wc -l",
+      switch(tools::file_ext(src.path),
+             "gz" = "gzcat",
+             "cat"),
+      src.path)
+    .max <- system(.cmd, intern = TRUE)
+    pb <- txtProgressBar(max = as.integer(.max))
+  }
   open(src)
   repeat {
-    .src <- readLines(src, n = 1)
+    .src <- readLines(src, n = 1000)
     if (length(.src) == 0) break
-    .obj <- fromJSON(.src, simplifyVector = TRUE, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
-    .dst <- vectorize(.obj, TRUE, TRUE)
-    .json <- toJSON(.dst, digits = 6)
+    .obj <- lapply(.src, fromJSON, simplifyVector = TRUE, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
+    .dst <- lapply(.obj, vectorize, hash = TRUE, sort = TRUE)
+    .json <- sapply(.dst, toJSON, digits = 6)
     write(.json, file = dst)
+    if (interactive()) setTxtProgressBar(pb, getTxtProgressBar(pb) + length(.src))
   }
   close(src)
   close(dst)
+  if (interactive()) close(pb)
 }
 
 for(fname in ndlist) {
