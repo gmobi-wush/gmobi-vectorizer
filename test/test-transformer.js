edd8ad47e34cdbd4b51a952dd6ab531c918b2759
wush.wu <wush.wu@generalmobi.com>
2016-04-20 18:00:47 +0800

    implement transformer: required

---
+++ test/test-transformer.js
@@ -0,0 +1,41 @@
+const
+  _ = require('underscore'),
+  fs = require('fs'),
+  path = require('path'),
+  zlib = require('zlib'),
+  es = require('event-stream'),
+  vectorizer = require('../');
+
+describe("Test that Transformer transform the request object", function() {
+
+  var schemas = _.filter(fs.readdirSync("test/schema"), function(s) {
+    return /json$/.test(s);
+  });
+  
+  _.map(schemas, function(fname) {
+    it(fname, function(done) {
+      var schemas = require('./schema/' + fname);
+      var transformer = new vectorizer.Transformer();
+      transformer.initialize(schemas);
+      var spath = './test/ndjson/' + fname.replace('json', 'ndjson');
+      var dpath = './test-sync/js/' + fname.replace('json', 'transform.ndjson');
+      var d = fs.createWriteStream(dpath);
+      var s0 = fs.createReadStream(spath);
+      var s = s0.pipe(es.split())
+        .pipe(es.map(function(line, cb) {
+          var result;
+          if (line.length > 0) {
+            var obj = JSON.parse(line);
+            debugger;
+            obj = transformer.transform(obj);
+          }
+        cb(null, "");
+      }))
+        .pipe(d);
+      s.on('finish', function() {
+        done();
+      });
+    });
+  });
+
+});
--- transformer.js
+++ transformer.js
@@ -1,18 +1,60 @@
 const 
-  check = require('check-types');
+  _ = require('underscore');
 
-function Transformer() {
-  return this;
+function validateProperty(obj, property) {
+  if (!_.has(obj, property)) throw new Error("The object does not has property: " + property);
 }
 
-Transformer.prototype = {
-  run : function(obj) {
-    if (check.object(obj)) {
-      return obj;
-    } else {
-      throw new Error("obj is not an object");
-    }
-  }
+// Constructor
+function Transformer(schemas) {
+  // initialization
+  this.transformersList = [];
+}
+
+// class methods
+Transformer.prototype.push = function(transformer) {
+  this.transformersList.push(transformer);
 };
 
+Transformer.prototype.initialize = function(schemas) {
+  // validate schemas
+  if (!_.isArray(schemas)) new Error("schema should be an array of objects");
+  _.forEach(schemas, function(schema) {
+    validateProperty(schema, "required");
+  });
+
+  var $ = this;
+  // generating trnasformer from schema
+  _.forEach(schemas, function(schema) {
+    _.forEach(schema.required, function(property) {
+      Transformer.factories;
+      $.transformersList.push(Transformer.factories.required(property));
+    });
+  });
+};
+
+Transformer.prototype.transform = function(obj) {
+  _.forEach(this.transformersList, function(transformer) {
+    transformer.transform(obj);
+  });
+  return obj;
+};
+
+Transformer.factories = {};
+
+Transformer.factories.required = function(property) {
+  var propertyPath = property.split("\u0002");
+  return ({
+    transform : function(obj) {
+      var currentObj = obj;
+      _.forEach(propertyPath, function(p) {
+        validateProperty(currentObj, p);
+        currentObj = currentObj[p];
+      });
+      return obj;
+    }
+  });
+};
+
+// export the class
 exports.Transformer = Transformer;
