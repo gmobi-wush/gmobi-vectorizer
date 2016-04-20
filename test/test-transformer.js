086080a1b5abeac69cf028135f5b5d100b59bae5
wush.wu <wush.wu@generalmobi.com>
2016-04-20 18:33:39 +0800

    test throwing

--- test/test-transformer.js
+++ test/test-transformer.js
@@ -39,3 +39,37 @@ describe("Test that Transformer transform the request object", function() {
   });
 
 });
+
+describe.only("Test that Required Transformer throws error if the property is not existed", function() {
+  var fname = "example.json";
+  it(fname, function(done) {
+    var schemas = require("./error-schema/" + fname);
+    var transformer = new vectorizer.Transformer();
+    transformer.initialize(schemas);
+    var spath = './test/ndjson/' + fname.replace('json', 'ndjson');
+    var s0 = fs.createReadStream(spath);
+    var index = 0;
+    var s = s0.pipe(es.split())
+      .pipe(es.map(function(line, cb) {
+        var result;
+        if (line.length > 0) {
+          var obj = JSON.parse(line);
+          if (index == 0) {
+            (function() {
+              transformer.transform(obj);
+            }).should.not.throw();
+            index += 1;
+          } else {
+            (function() {
+              transformer.transform(obj);
+            }).should.throw(/city/);
+          }
+        }
+        cb(null, "");
+      }));
+    s.on('end', function() {
+      done();
+    });
+  });
+  
+});
