98adfa86d0449d325531983758da11f0308f17bf
wush.wu <wush.wu@generalmobi.com>
2016-07-09 11:02:28 +0800

    refactor: template of check and benchmark

--- test/test-compatibility.js
+++ test/test-compatibility.js
@@ -1,114 +1,20 @@
 const
-  _ = require('underscore'),
-  fs = require('fs'),
-  path = require('path'),
-  zlib = require('zlib'),
-  es = require('event-stream'),
-  assert = require('assert'),
   vectorizer = require('../'),
-  check = require('check-types'),
-  crypto = require('crypto');
+  compatibility = require('./compatibility.js');
 
-function getMD5(path, cb) {
-  var hash = crypto.createHash('md5');
-  hash.setEncoding('hex');
-  return fs.createReadStream(path)
-    .pipe(hash)
-    .on('finish', function() {
-      hash.end();
-      return cb(null, hash.read());
-    });
-}
-
-describe("Test that the module creates the same object as the one online", function() {
-  it("Test the test/ndjson/bid.ndjson.gz without hash", function(done) {
+compatibility.testAndEval(
+  function() {
     var schema = require(__dirname + '/schema/bid.json');
     var transformer = new vectorizer.Transformer();
     transformer.initialize(schema);
-    return fs.createReadStream("test/ndjson/bid.ndjson.gz").pipe(zlib.createGunzip())
-      .pipe(es.split())
-      .pipe(es.map(function(line, cb) {
-        var obj;
-        try {
-          obj = {
-            bids : JSON.parse(line)
-          };
-        } catch(ex) {
-          return cb(null, "");
-        }
-        var retval = [];
-        for(var j = 0;j < 100;j++) {
-          obj.bids.ad = {
-            id : "56fce4c2ac79e5824c45608e" + j,
-            cid : "56fce40aac79e5824c456086" + j
-          };
-          var features = transformer.transform(obj);
-          var result = vectorizer.vectorize(features, false, function(err) { });
-          var index = [];
-          for(i = 0;i < result.i.length;i++) {
-            index.push(i);
-          }
-          index = _.sortBy(index, function(i) {return result.i[i];});
-          retval.push(JSON.stringify({
-            i: _.map(index, function(i) {return result.i[i];}),
-            x: _.map(index, function(i) {return result.x[i];})
-          }));
-        }
-        return cb(null, retval.join("\n") + "\n");
-      }))
-      .pipe(fs.createWriteStream("bid.vectorized.ndjson"))
-      .on('finish', function() {
-        return getMD5("bid.vectorized.ndjson", function(err, result) {
-          if (err) throw err;
-          assert(result === "2e41f578c5fe3b2e45f05344fcd11081");
-          return done();
-        });
-      });
-  });
-
-  it("Test the test/ndjson/bid.ndjson.gz with hash", function(done) {
-    var schema = require(__dirname + '/schema/bid.json');
-    var transformer = new vectorizer.Transformer();
-    transformer.initialize(schema);
-    return fs.createReadStream("test/ndjson/bid.ndjson.gz").pipe(zlib.createGunzip())
-      .pipe(es.split())
-      .pipe(es.map(function(line, cb) {
-        var obj;
-        try {
-          obj = {
-            bids : JSON.parse(line)
-          };
-        } catch(ex) {
-          return cb(null, "");
-        }
-        var retval = [];
-        for(var j = 0;j < 100;j++) {
-          obj.bids.ad = {
-            id : "56fce4c2ac79e5824c45608e" + j,
-            cid : "56fce40aac79e5824c456086" + j
-          };
-          var features = transformer.transform(obj);
-          var result = vectorizer.vectorize(features, true, function(err) { });
-          var index = [];
-          for(i = 0;i < result.i.length;i++) {
-            index.push(i);
-          }
-          index = _.sortBy(index, function(i) {return result.i[i];});
-          retval.push(JSON.stringify({
-            i: _.map(index, function(i) {return result.i[i];}),
-            x: _.map(index, function(i) {return result.x[i];})
-          }));
-        }
-        return cb(null, retval.join("\n") + "\n");
-      }))
-      .pipe(fs.createWriteStream("bid.vectorized.hashed.ndjson"))
-      .on('finish', function() {
-        return getMD5("bid.vectorized.hashed.ndjson", function(err, result) {
-          if (err) throw err;
-          assert(result === "46e919d196d93ae76b51c441a9784b19");
-          return done();
-        });
-      });
-  });
-
-});
+    return {
+      transformer : transformer
+    };
+  },
+  function(prepareObj, indexOfCampaign, obj, isHash) {
+    var features = prepareObj.transformer.transform(obj);
+    return vectorizer.vectorize(features, isHash, function(err) { });
+  },
+  "Use v0.1.5 Hasher",
+  null
+)();
